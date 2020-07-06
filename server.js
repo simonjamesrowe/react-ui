d52d93e67c5134b04400dddfe7ca8b0d5b1e3fd8
Simon Rowe <simon.rowe@gmail.com>
2020-07-06 20:40:09 +0100

    fix express and react routes.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/environment/config.js', function (req, res) {
     res.sendFile(path.join(__dirname, 'build/environment', 'config.js'));
 });
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
--- src/services/Environment.ts
+++ src/services/Environment.ts
@@ -6,7 +6,7 @@ interface IAppProps {
 
 const properties: IAppProps = {
     // @ts-ignore
-    apiUrl: window.API_URL || "http://localhost:8080"
+    apiUrl: window.API_URL || "https://api.simonjamesrowe.com"
 }
 
 // @ts-ignore
