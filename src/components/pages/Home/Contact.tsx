b0c3156db9517209d058ed93a0007da1054ef3a8
Simon Rowe <simon.rowe@gmail.com>
2021-02-15 08:05:59 +0000

    Implement the contact form

--- src/components/pages/Home/Contact.tsx
+++ src/components/pages/Home/Contact.tsx
@@ -1,14 +1,76 @@
 import React from "react";
-import {faMapPin,faPhone,faMailBulk} from "@fortawesome/free-solid-svg-icons";
+import {faMapPin, faPhone, faMailBulk} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {IProfile} from "../../../model/Profile";
+import {useFormik} from 'formik';
+import {Alert, Toast} from "react-bootstrap";
+import Axios from "axios";
+import {properties} from "../../../services/Environment";
 
 interface IProps {
     profile: IProfile
 }
-const Contact = ({profile} : IProps) => {
+
+const validate = values => {
+    const errors: { [key: string]: any } = {};
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    }
+
+    if (!values.lastName) {
+        errors.lastName = 'Required';
+    }
+
+    if (!values.emailAddress) {
+        errors.emailAddress = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.emailAddress)) {
+        errors.emailAddress = 'Invalid email address';
+    }
+
+    if (!values.subject) {
+        errors.subject = 'Required';
+    }
+
+    if (!values.content) {
+        errors.content = 'Required';
+    }
+
+    return errors;
+};
+
+const Contact = ({profile}: IProps) => {
+    const [showAlert, setShowAlert] = React.useState<boolean>(false);
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [success, setSuccess] = React.useState<boolean>(false);
+    const formik = useFormik({
+        initialValues: {
+            "firstName": "",
+            "lastName": "",
+            "emailAddress": "",
+            "subject": "",
+            "content": ""
+        },
+        validate,
+        onSubmit: (values) => {
+            setLoading(true);
+            Axios.post(`${properties.apiUrl}/contact-us`, values).then(
+                result => {
+                    if (result.status == 200) {
+                        setSuccess(true);
+                        formik.resetForm();
+                    } else {
+                        setSuccess(false);
+                    }
+                    setShowAlert(true);
+                    setTimeout(() => {setShowAlert(false)}, 3000);
+                    setLoading(false);
+                }
+            )
+        }
+    });
     return (
         <>
+
             <div className="port_contact_wrapper prt_toppadder80 page_scroll" data-scroll="3" id="contact_sec">
                 <div className="container">
                     <div className="row">
@@ -30,52 +92,76 @@ const Contact = ({profile} : IProps) => {
                                 <div className="row">
                                     <div className="col-md-12 text-center prt_bottompadder40 ">
                                         <div className="port_hadding_style_sec ">
-                                            <h2 className="port_head_wrapper">My Contact</h2>
+                                            <h2 className="port_head_wrapper">Send a message</h2>
                                         </div>
+                                        <Alert variant={success ? "success" : "danger"} onClose={() => setShowAlert(false)} show={showAlert} dismissible>
+                                            <p>{success ? "Your message has been sent" : "An unexpected error has occured"}</p>
+                                        </Alert>
                                     </div>
                                     <div className="col-12">
+
                                         <div className="port_contact_form">
-                                            <form>
+                                            <form onSubmit={formik.handleSubmit}>
                                                 <div className="row">
                                                     <div className="col-sm-6">
                                                         <div className="form-group">
-                                                            <input type="text" name="first_name"
-                                                                   className="form-control require" id="name"
+                                                            <input type="text" name="firstName"
+                                                                   className="form-control require" id="firstName"
+                                                                   value={formik.values.firstName}
+                                                                   onChange={formik.handleChange}
                                                                    placeholder="First name"/>
+                                                            {formik.touched.firstName && formik.errors.firstName ? <div
+                                                                className="error">{formik.errors.firstName}</div> : null}
                                                         </div>
                                                     </div>
                                                     <div className="col-sm-6">
                                                         <div className="form-group">
-                                                            <input type="text" name="last_name"
+                                                            <input type="text" name="lastName"
+                                                                   value={formik.values.lastName}
+                                                                   onChange={formik.handleChange}
                                                                    className="form-control require" id="last-name"
                                                                    placeholder="Last name"/>
+                                                            {formik.touched.lastName && formik.errors.lastName ? <div
+                                                                className="error">{formik.errors.lastName}</div> : null}
                                                         </div>
                                                     </div>
                                                     <div className="col-sm-6">
                                                         <div className="form-group">
-                                                            <input type="email" name="email"
+                                                            <input type="email" name="emailAddress"
+                                                                   value={formik.values.emailAddress}
+                                                                   onChange={formik.handleChange}
                                                                    className="form-control require" id="email"
                                                                    placeholder="Email Address" data-valid="email"
                                                                    data-error="Email should be valid."/>
+                                                            {formik.touched.emailAddress && formik.errors.emailAddress ? <div
+                                                                className="error">{formik.errors.emailAddress}</div> : null}
                                                         </div>
                                                     </div>
                                                     <div className="col-sm-6">
                                                         <div className="form-group">
                                                             <input type="text" name="subject"
+                                                                   value={formik.values.subject}
+                                                                   onChange={formik.handleChange}
                                                                    className="form-control require" id="subject"
                                                                    placeholder="Subject"/>
+                                                            {formik.touched.subject && formik.errors.subject ? <div
+                                                                className="error">{formik.errors.subject}</div> : null}
                                                         </div>
                                                     </div>
                                                     <div className="col-12">
                                                         <div className="form-group">
-                                                            <textarea name="message" className="form-control require"
-                                                                      rows={3} id="comment"
+                                                            <textarea name="content" className="form-control require"
+                                                                      value={formik.values.content}
+                                                                      onChange={formik.handleChange}
+                                                                      rows={3} id="content"
                                                                       placeholder="Message"></textarea>
+                                                            {formik.touched.content && formik.errors.content ? <div
+                                                                className="error">{formik.errors.content}</div> : null}
                                                         </div>
                                                     </div>
                                                     <div className="col-12">
                                                         <div className="banner_btn">
-                                                            <button type="button" data-type="contact"
+                                                            <button disabled={loading} type="submit" data-type="contact"
                                                                     className="portfolio_btn btn_red submitForm">
                                                                 <span className="first_text">Submit</span>
                                                                 <span className="second_text">Send</span>
@@ -100,7 +186,7 @@ const Contact = ({profile} : IProps) => {
                                     <div className="col-sm-4 text-center">
                                         <div className="contact_info">
                                             <div className="info_img">
-                                                <FontAwesomeIcon icon={faMapPin} size={"3x"} />
+                                                <FontAwesomeIcon icon={faMapPin} size={"3x"}/>
                                             </div>
                                             <h2 className="footer_heading">Location</h2>
                                             <p>{profile.location}</p>
@@ -109,7 +195,7 @@ const Contact = ({profile} : IProps) => {
                                     <div className="col-sm-4 text-center">
                                         <div className="contact_info">
                                             <div className="info_img">
-                                                <FontAwesomeIcon icon={faPhone} size={"3x"} />
+                                                <FontAwesomeIcon icon={faPhone} size={"3x"}/>
                                             </div>
                                             <h2 className="footer_heading">Phone</h2>
                                             <p><a href="javascript:;">{profile.phoneNumber}</a></p>
@@ -119,7 +205,7 @@ const Contact = ({profile} : IProps) => {
                                     <div className="col-sm-4 text-center">
                                         <div className="contact_info">
                                             <div className="info_img">
-                                                <FontAwesomeIcon icon={faMailBulk} size={"3x"} />
+                                                <FontAwesomeIcon icon={faMailBulk} size={"3x"}/>
                                             </div>
                                             <h2 className="footer_heading">Email</h2>
                                             <p><a href="javascript:;">{profile.primaryEmail}</a></p>
